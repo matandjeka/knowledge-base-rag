0ba2290ceb9ce83c8cf0dba48a91022a70c222b0
matandjeka <latandjeka@gmail.com>
2026-09-13 23:05:52 -0400

    remove  X-Frame-Options: DENY

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -7,7 +7,6 @@ const config: NextConfig = {
     return [{source: "/:path*", headers: [
       {key: "X-Content-Type-Options", value: "nosniff"},
       {key: "Referrer-Policy", value: "no-referrer"},
-      {key: "X-Frame-Options", value: "DENY"},
     ]}];
   },
 };
